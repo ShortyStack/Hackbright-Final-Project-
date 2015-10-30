3527ac545b651e90cfef78e1b06bd1eea6353cfc
wendy <wendy@zenone.org>
2015-10-29 17:34:51 -0700

    first commit. Added files and seeded db

---
+++ model.py
@@ -0,0 +1,92 @@
+# coding=utf-8
+
+
+"""
+This file purely defines the schema used in my database.
+"""
+
+
+# Things I needed to pip install
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+# Connection to our database
+db = SQLAlchemy()
+
+
+##############################################################################
+# Model definitions
+
+class User(db.Model):
+    """User of Netflix and Chow site"""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String(64), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    street_address = db.Column(db.Text, nullable=False)
+    zipcode = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+        return "<User user_id={} email={}>".format(self.user_id, self.email)
+
+    def hash_password(self, password):
+        """Used for setting a hashed+salted password.
+        :param password:"""
+
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+
+        """Used to validate that passwods match
+        :param password:
+        :return:"""
+
+        return check_password_hash(self.password_hash, password)
+
+
+class Audit(db.Model):
+    """This is the audit log"""
+
+    __tablename__ = "auditing"
+
+    event_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+    ip = db.Column(db.Text, nullable=False)
+    user_agent = db.Column(db.Text, nullable=False)
+    event = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        """
+        Provide helpful representation when printed.
+        """
+
+        return "<event_id={} User user_id={} event={}>".format(self.event_id,
+                                                               self.user_id,
+                                                               self.email)
+
+
+##############################################################################
+# Helper functions
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our SQLite database. Enter sqlite3 netflixandchow.db
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///netflixandchow.db'
+    db.app = app
+    db.init_app(app)
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print "Connected to DB."
---
+++ seed.py
@@ -0,0 +1,75 @@
+"""Utility file to seed ratings database from MovieLens data in seed_data/"""
+#seed.py populates the tables. 
+
+from model import User
+# from model import Audit
+
+
+from model import connect_to_db, db
+from routes import app
+
+from datetime import datetime 
+
+
+def load_users():
+    """Load users from user.csv into database."""
+    #open file
+    user_csv = open("user.csv")
+    # Tnterate through the lines
+    # This will put the data into a list
+    for line in user_csv:
+        line_list = line.strip().split("|")
+    # Another for loop to iterate through
+        for i in range(len(line_list)):
+            line_list[i] = line_list[i].strip()
+        user_id, email, password, street_address, zipcode = line_list[0], line_list[1], line_list[2], line_list[3], line_list[4]
+        print "USER_ID %s, EMAIL: %s, PASSWORD: %s, STREET_ADDRESS: %s, ZIPCODE: %s" % (user_id, email, password, street_address, zipcode)
+
+        user = User(user_id=user_id, email=email, password=password, street_address=street_address, zipcode=zipcode)
+
+        db.session.add(user)
+    db.session.commit()
+
+
+# def load_auditing():
+#     """Load audit log from u.audit into database."""
+#     print "Audit Log"
+
+#     Audit.query.delete()
+
+#     for row in open("seed_data/u.movies"):
+#         #stripping on the right side to remove whitespace
+#         print row
+#         #row = row.strip()
+#         #splitting by the ('|')
+#         movie_id, title, released_at, imdb_url = row.split("|")[:4]
+
+#         title = title[:-7]
+
+#         if released_at == "":
+#             released_at = None
+#         else:
+#             released_at = datetime.strptime(released_at, "%d-%b-%Y")
+
+
+
+#         # now we will create the table
+#         # created an instance of of the movie class and importing it from Movie in model.py   
+#         # Movie is (Cat) movie is (Auden) = movie_id, title..is(hunger=10)
+#         movie = Movie(movie_id=movie_id,
+#                        title=title,
+#                        released_at=released_at,
+#                        imdb_url=imdb_url)
+#         # we need to add to the session or it won't be stored
+#         db.session.add(movie)
+
+#     #once we are done, we should commit our work
+#     db.session.commit()
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    # In case tables haven't been created, create them
+    db.create_all()
+
+    # Import different types of data
+    # load_users()
